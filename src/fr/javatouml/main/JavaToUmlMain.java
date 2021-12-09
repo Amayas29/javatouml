093af2d5ecdcfc07ddafbff33c65c31d50debe5d
Amayas29 <amayas29sd@gmail.com>
2021-12-09 23:48:29 +0100

    add lib

--- src/fr/javatouml/main/JavaToUmlMain.java
+++ src/fr/javatouml/main/JavaToUmlMain.java
@@ -4,15 +4,15 @@ import fr.javatouml.parser.Parser;
 
 public class JavaToUmlMain {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        if (args.length != 1) {
-            System.err.println("<Invalid number of arguments> Usage : java Parser <java filename>");
-            System.exit(1);
-        }
+		if (args.length != 1) {
+			System.err.println("<Invalid number of arguments> Usage : java Parser <java filename>");
+			System.exit(1);
+		}
 
-        Parser parser = new Parser(args[0]);
-        parser.parse(args[0].substring(0, args[0].length() - 5));
-    }
+		Parser parser = new Parser(args[0]);
+		parser.parse(args[0].substring(0, args[0].length() - 5));
+	}
 
 }
--- src/fr/javatouml/uml/InterfaceSection.java
+++ src/fr/javatouml/uml/InterfaceSection.java
@@ -5,91 +5,91 @@ import java.util.List;
 
 public class InterfaceSection implements Section {
 
-    private String name;
-    private List<String> interfaces;
-    private List<Section> sections;
+	private String name;
+	private List<String> interfaces;
+	private List<Section> sections;
 
-    public InterfaceSection(String name) {
-        this.name = name;
-        interfaces = new ArrayList<>();
-        sections = new ArrayList<>();
-    }
+	public InterfaceSection(String name) {
+		this.name = name;
+		interfaces = new ArrayList<>();
+		sections = new ArrayList<>();
+	}
 
-    @Override
-    public String getVisibility() {
-        return "";
-    }
+	@Override
+	public String getVisibility() {
+		return "";
+	}
 
-    @Override
-    public String getName() {
-        return name.concat(" ");
-    }
+	@Override
+	public String getName() {
+		return name.concat(" ");
+	}
 
-    @Override
-    public String getType() {
-        return "interface ";
-    }
+	@Override
+	public String getType() {
+		return "interface ";
+	}
 
-    @Override
-    public String ifAbstract() {
-        return "";
-    }
+	@Override
+	public String ifAbstract() {
+		return "";
+	}
 
-    @Override
-    public String ifStatic() {
-        return "";
-    }
+	@Override
+	public String ifStatic() {
+		return "";
+	}
 
-    public void extendsSection(String section) {
-        if (section == null)
-            return;
+	public void extendsSection(String section) {
+		if (section == null)
+			return;
 
-        interfaces.add(section);
-    }
+		interfaces.add(section);
+	}
 
-    public void addSection(Section section) {
-        if (section == null || section instanceof InterfaceSection)
-            return;
+	public void addSection(Section section) {
+		if (section == null || section instanceof InterfaceSection)
+			return;
 
-        sections.add(section);
-    }
+		sections.add(section);
+	}
 
-    @Override
-    public String toPlantUmlString() {
-        StringBuilder sb = new StringBuilder(Uml.toBaseUml(this));
-        StringBuilder supB = new StringBuilder();
+	@Override
+	public String toPlantUmlString() {
+		StringBuilder sb = new StringBuilder(Uml.toBaseUml(this));
+		StringBuilder supB = new StringBuilder();
 
-        for (int i = 0; i < interfaces.size(); i++) {
+		for (int i = 0; i < interfaces.size(); i++) {
 
-            String inter = interfaces.get(i);
+			String inter = interfaces.get(i);
 
-            if (supB.isEmpty())
-                supB.append("extends ");
+			if (supB.length() == 0)
+				supB.append("extends ");
 
-            supB.append(inter);
+			supB.append(inter);
 
-            if (i != interfaces.size() - 1)
-                supB.append(", ");
-        }
+			if (i != interfaces.size() - 1)
+				supB.append(", ");
+		}
 
-        supB.append(" ");
-        String sup = supB.toString();
+		supB.append(" ");
+		String sup = supB.toString();
 
-        sb.append(sup).append("{\n");
+		sb.append(sup).append("{\n");
 
-        for (Section section : sections)
-            sb.append(section.toPlantUmlString()).append("\n");
+		for (Section section : sections)
+			sb.append(section.toPlantUmlString()).append("\n");
 
-        sb.append("}\n");
+		sb.append("}\n");
 
-        return sb.toString();
-    }
+		return sb.toString();
+	}
 
-    public List<String> getInterfaces() {
-        return new ArrayList<>(interfaces);
-    }
+	public List<String> getInterfaces() {
+		return new ArrayList<>(interfaces);
+	}
 
-    public List<Section> getSections() {
-        return new ArrayList<>(sections);
-    }
+	public List<Section> getSections() {
+		return new ArrayList<>(sections);
+	}
 }
